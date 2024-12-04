70091bb8a82fa7491e4b550504c8131e758911e0
Omkaar Shenoy <omkaarshenoyos@gmail.com>
2024-12-03 21:47:35 -0700

    added requirements

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:@localhost:5432/eventology_db"
+# DATABASE_URL = "postgresql://postgres:@localhost:5432/eventology_db"
+DATABASE_URL = "postgresql://eventology_db_owner:YXeow01pgUAH@ep-rapid-brook-a55mgqfg.us-east-2.aws.neon.tech/eventology_db?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
